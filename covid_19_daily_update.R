d039f7ecef3c420bfbdc567e289331f56a64251b
casinocullen <casinocullen@gmail.com>
2020-03-31 16:16:09 +0000

    Make the layer to fit crontab

--- covid_19_daily_update.R
+++ covid_19_daily_update.R
@@ -6,16 +6,39 @@
 
 # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 # SETUP  ----
-
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path)); getwd();
-
 # FUNCTIONS
-source("../base/global.R")
+#source("/data/Github/base/global.R")
+require(tidyverse)
+require(sf)
+require(DBI)
+source("/data/Github/base/functions/write_layer_absolute.R")
+
+# cori-risi connection
+#   host: cori-risi
+#     DB: data
+# Schema: layer
+
+coririsiconf <- config::get("cori-risi", file = "/data/Github/base/config.yml")
+coririsi_layer = dbConnect(
+  RPostgres::Postgres(),
+  user     = coririsiconf$user,
+  password = coririsiconf$password,
+  dbname   = coririsiconf$dbname,
+  host     = coririsiconf$host,
+  port     = coririsiconf$port,
+  options  =  '-c search_path=sch_layer,public'
+)
+#dbListTables(coririsi)
+
+rm(coririsiconf)
+
+
+
 x.date <- as.character(Sys.Date(), format = '%m_%d_%Y')
 
 st <- as.Date("2020-3-22")
 en <- Sys.Date()
-all.dates <- as.character(seq(en, st, by = "-1 day"), format = '%m-%d-%Y')
+all.dates <- as.character(seq(st, en, by = "+1 day"), format = '%m-%d-%Y')
 
 x.geo.county = st_read(coririsi_layer, "geo_attr_county_pg")
 
@@ -34,12 +57,22 @@ for (date in all.dates) {
       dplyr::mutate(!!paste0("confirmed_", date) := Confirmed, 
                     !!paste0("deaths_", date):= Deaths, 
                     !!paste0("recovered_", date):= Recovered, 
-                    !!paste0("active_", date):= Active) %>%
-      dplyr::select(geoid, name, st_stusps, starts_with("confirmed_"), starts_with("deaths_"), starts_with("recovered_"), starts_with("active_")) %>% 
+                    !!paste0("active_", date):= Active,
+                    latest_update = date) %>%
+      dplyr::select(geoid, name, st_stusps, latest_update, starts_with("confirmed_"), starts_with("deaths_"), starts_with("recovered_"), starts_with("active_")) %>% 
       janitor::clean_names()
+    
+    latest_update = gsub("-", "_", date)
   }
 }
 
+x.geo.county.covid.latest = x.geo.county.covid %>% 
+  dplyr::select(geoid, name, st_stusps, latest_update, ends_with(latest_update)) %>% 
+  dplyr::rename(confirm := !!paste0("confirmed_", latest_update), 
+                deaths := !!paste0("deaths_", latest_update), 
+                recovered := !!paste0("recovered_", latest_update), 
+                active := !!paste0("active_", latest_update))
+
 # write
-write_layer(x.geo.county.covid, paste0("covid_19_county_", x.date), db = T)
-write_layer(x.geo.county.covid, "covid_19_county_latest", db = T)
+write_layer_absolute(x.geo.county.covid, paste0("covid_19_county_", x.date), db = T, fs = T)
+write_layer_absolute(x.geo.county.covid.latest, "covid_19_county_latest", db = T, fs = T, ngacarto = T, ngacarto.overwrite = T, new.server = T, new.server.overwrite = T)
