ee82da66ea837909878a9dc40df780ffd1e86c08
casinocullen <casinocullen@gmail.com>
2020-04-04 17:01:17 +0000

    Add drive time

--- hospital_staff.R
+++ hospital_staff.R
@@ -7,7 +7,8 @@ rm(list=ls(pattern=("(^x\\.)")))
 
 source("../base/global.R")
 
-
+# Raw data
+# TODO: add to database
 url_to_s3(url = "http://download.cms.gov/nppes/NPPES_Data_Dissemination_March_2020.zip", 
           filename = "NPPES_Data_Dissemination_March_2020.zip", 
           s3path = "source/proj-covid-19/", 
@@ -31,7 +32,7 @@ x.npi.grp = x.npi %>%
   dplyr::summarise(count = n(npi))
 
 ss = "https://docs.google.com/spreadsheets/d/1tMPN1A60ixs6brmOW7ccp194UnHJgsw3VWOasZHy9r8/edit#gid=0"
-x.list.taxnomy = read_sheet(ss) %>% 
+x.list.taxnomy = read_sheet(ss, sheet = "critical hospital staff categorization") %>% 
   dplyr::filter(`RG Use?` == "Y") %>%
   dplyr::select(taxonomycode)
 
@@ -39,7 +40,7 @@ names(x.list.taxnomy)
 
 x.npi.filter = x.npi %>% 
   dplyr::filter(taxonomycode %in% x.list.taxnomy[['taxonomycode']]) %>% 
-  st_as_sf(coords = c('lon', 'lat'), crs = 4269)
+  st_as_sf(coords = c('lon', 'lat'), crs = 4269, remove = F)
                   
                   
 x.attr.county = st_read(coririsi_layer, 'geo_attr_county_pg') %>% 
@@ -62,4 +63,44 @@ x.npi.filter.grp = x.npi.filter %>%
   dplyr::summarise(count = n(npi))
 
 write_layer(x.npi.filter.county, "hospital_critical_staff_by_county", db = T, new.server = T, new.server.overwrite = T, ngacarto = T, ngacarto.overwrite = T)
-                  
+
+# Update: 4/4
+# Est. drive-time
+
+x.npi.filter = x.npi.filter %>% st_drop_geometry()
+
+x.npi.filter.batch.1 = x.npi.filter[c(1:20000),]
+x.npi.filter.batch.2 = x.npi.filter[c(20001:40000),]
+saveRDS(x.npi.filter.batch.2, "./output/x.npi.filter.batch.2.RDS")
+x.npi.filter.batch.3 = x.npi.filter[c(40001:60000),]
+saveRDS(x.npi.filter.batch.3, "./output/x.npi.filter.batch.3.RDS")
+x.npi.filter.batch.4 = x.npi.filter[c(60001:80000),]
+saveRDS(x.npi.filter.batch.4, "./output/x.npi.filter.batch.4.RDS")
+x.npi.filter.batch.5 = x.npi.filter[c(80001:98387),]
+saveRDS(x.npi.filter.batch.5,"./output/x.npi.filter.batch.5.RDS")
+
+
+x.npi.filter.dt = NULL
+i = 1
+while (i <= nrow(x.npi.filter.batch.1)) {
+  onepoint_info = x.npi.filter.batch.1[i, ]
+  onepoint_info_expanded <- onepoint_info[rep(row.names(onepoint_info), 2), ]
+  
+  onepoint = NULL
+  while(is.null(onepoint)) {
+    try(
+      onepoint <- osrmIsochrone(loc = c(x.npi.filter.batch.1[i,"lon"], x.npi.filter.batch.1[i,"lat"]), breaks = c(40,75),
+                                returnclass="sf") %>% 
+        dplyr::select(driveTime = max, geometry))
+    
+  }
+  if(nrow(onepoint) == 2){
+    print(paste0("SUCCESSFULLY INPUT ", x.npi.filter.batch.1[i,"npi"]))
+    onepoint_info_expanded = bind_cols(onepoint_info_expanded, onepoint)
+    x.npi.filter.dt = rbind(x.npi.filter.dt, onepoint_info_expanded)
+    i = i + 1
+  }
+}
+
+
+
