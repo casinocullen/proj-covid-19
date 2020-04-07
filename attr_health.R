a9ce54eaabd2e187f5a6bacc48b10432a5e13598
casinocullen <casinocullen@gmail.com>
2020-04-07 16:39:10 +0000

    Add hospital/staff drive time

--- attr_health.R
+++ attr_health.R
@@ -6,20 +6,22 @@ rm(list=ls(pattern=("(^x\\.)")))
 
 
 source("../base/global.R")
+x.date <- as.character(Sys.Date(), format = '%m_%d_%Y')
 
 # County geo
-x.geo.county = st_read(coririsi_layer, 'geo_attr_county_pg') %>% 
-  dplyr::select(geoid)
+x.geo.county = st_read(coririsi_layer, 'geo_attr_county_pg') 
 
 # COVID-19 case
 x.layer.case = st_read(coririsi_layer, "covid_19_county_latest") %>% 
   st_drop_geometry() %>% 
   group_by(geoid) %>% 
-  dplyr::summarise_all(max)
+  dplyr::summarise_all(max) %>% 
+  dplyr::select(geoid, covid_19_latest_update = latest_update, confirm, deaths, recovered, active)
+
+names(x.layer.case)
 
 # Attr_health
-x.layer.county = st_read(coririsi_layer, "attr_county_health") %>% 
-  st_drop_geometry()
+x.layer.county = st_read(coririsi_layer, "acs_county_health") 
 
 # CDC SVI
 x.layer.svi = dbReadTable(coririsi_source, "cdc_svi") %>%
@@ -34,6 +36,31 @@ x.hospital.dt = st_read(coririsi_layer, "hospitals_drivetime_cori_pg") %>%
   dplyr::mutate(beds = ifelse(beds == -999, 0, beds),
                 hospital_name = name)
 
+x.hospital.dt.county = x.geo.county %>% 
+  st_join(x.hospital.dt) %>% 
+  dplyr::group_by(geoid) %>% 
+  dplyr::summarise(hospitals_name_40_mins = str_c(hospital_name, collapse = ", "),
+                   total_estimated_bed_40_mins = sum(beds)) %>% 
+  st_drop_geometry()
+
+# Hospital Points
+
+x.hospital.beds = st_read(coririsi_layer, "definitive_healthcare_hospital_beds") %>% 
+  dplyr::filter(HOSPITAL_T %in% c('Critical Access Hospital', 'VA Hospital', 'Short Term Acute Care Hospital')) 
+
+x.hospital.beds.county = x.hospital.beds %>% 
+  st_drop_geometry() %>%
+  group_by(geoid_co) %>% 
+  dplyr::summarise(hospital_count =n_distinct(OBJECTID), 
+                   hospitals_name = str_c(HOSPITAL_N, collapse = ", "),
+                   total_licensed_bed = sum(NUM_LICENS), 
+                   total_staffed_bed = sum(NUM_STAFFE), 
+                   total_icu_bed = sum(NUM_ICU_BE), 
+                   median_bed_utiliz = median(BED_UTILIZ), 
+                   potential_increase_beds = sum(Potential_)) %>%
+  ungroup()
+
+
 # ACS
 x.layer.acs = dbReadTable(coririsi_layer, "acs_county") %>% 
   dplyr::select(GEOID, unemployed_pct_2018, under_poverty_level_pct_2018, capita_income_past_12_months_2018)
@@ -42,30 +69,38 @@ x.layer.acs = dbReadTable(coririsi_layer, "acs_county") %>%
 x.layer.staff = st_read(coririsi_layer, "hospital_critical_staff_by_county") %>% 
   st_drop_geometry()
 
+# Staff Drivetime
+x.layer.staff.dt = st_read(coririsi_layer, "npi_drivetime_40_county") %>% 
+  st_drop_geometry()
+
+
 #IHME
-x.layer.ihme = st_read(coririsi_layer, "ihme_peak_dates") %>% 
+x.layer.ihme = st_read(coririsi_layer, paste0("ihme_peak_dates_",x.date)) %>% 
   st_drop_geometry() %>% 
   dplyr::select(geoid_st,  ends_with("_date"), ends_with("_needed"))
 
 
 # JOIN ALL TABLES
 x.geo.county.join.pg = x.geo.county %>% 
-  st_join(x.hospital.dt) %>% 
-  dplyr::group_by(geoid) %>% 
-  dplyr::summarise(hospitals_name_40_mins = str_c(hospital_name, collapse = ", "),
-                   total_estimated_bed_40_mins = sum(beds)) %>% 
+  left_join(x.hospital.dt.county, by = 'geoid') %>%
+  left_join(x.hospital.beds.county, by = c('geoid' = 'geoid_co')) %>%
   left_join(x.layer.county, by = 'geoid') %>%
   left_join(x.layer.staff, by = 'geoid') %>%
+  left_join(x.layer.staff.dt, by = 'geoid') %>%
   left_join(x.layer.acs, by = c('geoid' = 'GEOID')) %>%
   left_join(x.layer.svi, by = c('geoid' = 'FIPS')) %>% 
-  left_join(x.layer.case, by = c('geoid', 'st_stusps', 'name')) %>% 
+  left_join(x.layer.case, by = c('geoid')) %>% 
   left_join(x.layer.ihme, by = c('geoid_st')) %>% 
   dplyr::mutate(pop_density = total_population_2018/land_sqmi, 
                 confirm_pct = confirm/total_population_2018 * 100,
                 death_pct = deaths/confirm * 100,
                 all_beds_40_mins_per_1000 = total_estimated_bed_40_mins * 1000/total_population_2018,
                 pct_65_over_2018 = population_65_over_2018/total_population_2018 * 100, 
-                pct_85_over_2018 = population_85_over_2018/total_population_2018 * 100) %>% 
+                pct_85_over_2018 = population_85_over_2018/total_population_2018 * 100,                
+                icu_beds_per_1000 = round(total_icu_bed * 10000/total_population_2018)/10,
+                icu_beds_per_1000_elder = round(total_icu_bed * 10000/population_65_over_2018)/10,
+                icu_beds_per_100k = round(total_icu_bed * 100000/total_population_2018),
+                icu_beds_per_100k_elder = round(total_icu_bed * 100000/population_65_over_2018)) %>%
   dplyr::select(geoid, name, namelsad, st_stusps, 
                 confirm, deaths, 
                 total_staff,
